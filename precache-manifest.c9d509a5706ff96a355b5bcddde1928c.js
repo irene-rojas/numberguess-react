1f7fe75a46868e1c34f41f4ca5a5f7f79bf5eeda
Irene Rojas <irene@smallwondermedia.com>
2019-07-11 21:24:33 -0400

    Updates

---
+++ precache-manifest.c9d509a5706ff96a355b5bcddde1928c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f62f27bb698ba8f0a58055dffc87720",
+    "url": "/numberguess-react/static/media/yellow_cat.6f62f27b.png"
+  },
+  {
+    "revision": "ff6e9d8ecaa207eceac799c264a53a34",
+    "url": "/numberguess-react/static/media/gray_cat.ff6e9d8e.png"
+  },
+  {
+    "revision": "953720e2b686b92210756cd152204c01",
+    "url": "/numberguess-react/static/media/frame.953720e2.png"
+  },
+  {
+    "revision": "1f536498830cf81767946d908f2a432f",
+    "url": "/numberguess-react/static/media/brown_cat.1f536498.png"
+  },
+  {
+    "revision": "4b1eb2da72a3acdf6421d159d9f563a8",
+    "url": "/numberguess-react/static/media/black_cat.4b1eb2da.png"
+  },
+  {
+    "revision": "244d8d939bd5b4ed04bf",
+    "url": "/numberguess-react/static/js/runtime~main.244d8d93.js"
+  },
+  {
+    "revision": "67c142db145c8c578c05",
+    "url": "/numberguess-react/static/js/main.67c142db.chunk.js"
+  },
+  {
+    "revision": "4cf3317fae0870e0d15c",
+    "url": "/numberguess-react/static/js/1.4cf3317f.chunk.js"
+  },
+  {
+    "revision": "67c142db145c8c578c05",
+    "url": "/numberguess-react/static/css/main.b4b256c5.chunk.css"
+  },
+  {
+    "revision": "908b49add1fe6133d9e7e231758de2dc",
+    "url": "/numberguess-react/index.html"
+  }
+];
